786124ab6a77a27218da6450a838e38b359a5e86
Larissa da Rocha Nunes <larissadarochanunes47@gmail.com>
2026-05-13 15:13:23 -0300

    Html e Css da página de Login

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,21 +1,35 @@
-import{Input} from '../components/input'
+import{Input} from '../components/input';
+import imagemFundo from '../assets/fundo.png';
+import logo from '../assets/Logo.png';
 
 export function Login(){
     return (
-        <main>
-            <div>
-                <h1>Bem-Vindo de volta!</h1>
-                <p>Encontre parceiros para treinar ao ar livre.</p>
-                <p>Conecte-se e comece agora! 💪</p>
-                <form action="">
-                    <Input id="email" label="E-mail" type="email" placeholder="Ex: joao@email.com" />
-                    <Input id="password" label="Senha" type="password" placeholder="Ex: joao123" required/>
-                    <button type='submit'>Entrar</button>
-                </form>
-                <p>Ainda não tem uma conta? <a href="#">Cadastre-se</a></p>
-    
+        <main className='flex min-h-screen'>
+            <div className='hidden md:block w-full md:w-1/2'>
+                <img src={imagemFundo} alt="Pessoas se exercitando ao ar livre" className="w-full h-full object-cover object-center" />
+            </div>
+            <div className='w-full md:w-1/2 flex flex-col items-start justify-center p-8 md:p-16'>
+            <div className='className="flex items-center gap-2 mb-8 text-[#10B981]'>
+                <img src={logo} alt="Logotipo FitMeet"/>
 
             </div>
+            <div className='w-full max-w-sm'>
+
+                <h1 className='text-3xl font-display'>Bem-Vindo de volta!</h1>
+                <p className='text-sm text-[#374151] mb-1'>Encontre parceiros para treinar ao ar livre.</p>
+                <p className='text-sm text-[#374151] mb-8'>Conecte-se e comece agora! 💪</p>
+                <form action="" className="flex flex-col gap-6">
+                    <div className='mb-4'>
+                        <Input id="email" label="E-mail" type="email" placeholder="Ex: joao@email.com" required />
+                    </div>
+                    <div className='mb-8 text-wrap'>
+                        <Input id="password" label="Senha" type="password" placeholder="Ex: joao123" required/>
+                    </div>
+                        <button type='submit' className='w-full bg-[#10B981] cursor-pointer hover:bg-[#059669] text-white font-semibold py-3 px-6 rounded-md transition duration-200'>Entrar</button>
+                        </form>
+                        <p className='mt-8 text-sm text-[#6B7280] text-center'>Ainda não tem uma conta? <a href="#" className='font-medium text-[inherit] hover:underline'>Cadastre-se</a></p>
+                    </div>
+                </div>
         </main>
     )
 }
--- src/components/input.tsx
+++ src/components/input.tsx
@@ -10,7 +10,7 @@ export function Input({ label, type, placeholder, id, required=false }: InputPro
   return (
     <div className="input-field">
       <label htmlFor={id}>{label}</label>
-      <input type={type} id={id} placeholder={placeholder} required={required} />
+      <input type={type} id={id} placeholder={placeholder} required={required} className="w-full px-4 py-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[black] focus:border-transparent transition-all" />
     </div>
   );
 }
